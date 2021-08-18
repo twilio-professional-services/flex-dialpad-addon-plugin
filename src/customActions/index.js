ed205c69d4631ef5d418ae8ae399239dd1ce407c
Jared Hunter <jhunter@twilio.com>
2021-08-17 21:51:19 -0500

    Adding listener for HangupCall that ensures participants are no longer on hold when hanging up

--- src/customActions/index.js
+++ src/customActions/index.js
@@ -1,4 +1,4 @@
-import { Actions } from '@twilio/flex-ui';
+import { Actions, StateHelper } from '@twilio/flex-ui';
 import { acceptInternalTask, rejectInternalTask, isInternalCall, toggleHoldInternalCall } from './internalCall';
 import { kickExternalTransferParticipant } from './externalTransfer';
 import ConferenceService from '../helpers/ConferenceService';
@@ -7,45 +7,45 @@ export default (manager) => {
 
   Actions.addListener('beforeAcceptTask', (payload, abortFunction) => {
 
-      const reservation = payload.task.sourceObject;
+    const reservation = payload.task.sourceObject;
 
-      if(isInternalCall(payload)){
-        
-        abortFunction();
+    if (isInternalCall(payload)) {
 
-        acceptInternalTask({ reservation, payload });
-        
-      } 
+      abortFunction();
+
+      acceptInternalTask({ reservation, payload });
+
+    }
 
   })
 
   Actions.addListener('beforeRejectTask', (payload, abortFunction) => {
 
-      if (isInternalCall(payload)) {
+    if (isInternalCall(payload)) {
 
-        abortFunction();
+      abortFunction();
 
-        rejectInternalTask({ manager, payload });
-  
-      } 
+      rejectInternalTask({ manager, payload });
+
+    }
 
   })
 
   const holdCall = (payload, hold) => {
     return new Promise((resolve, reject) => {
-      
+
       const task = payload.task;
 
       if (isInternalCall(payload)) {
-        
-        toggleHoldInternalCall({ 
-          task, manager, hold, resolve, reject 
+
+        toggleHoldInternalCall({
+          task, manager, hold, resolve, reject
         });
-        
+
       } else {
 
         resolve();
-       
+
       }
 
     })
@@ -80,27 +80,58 @@ export default (manager) => {
     }
 
     console.log('Holding participant', participantSid);
-    
+
     const { conferenceSid } = task.conference;
     abortFunction();
     return ConferenceService.unholdParticipant(conferenceSid, participantSid);
   });
 
-	Actions.addListener('beforeKickParticipant', (payload, abortFunction) => {
+  Actions.addListener('beforeKickParticipant', (payload, abortFunction) => {
 
-      const { participantType } = payload;
-      
-      if (
-        participantType !== "transfer" &&
-        participantType !== 'worker'
-      ) {
-        
-        abortFunction();
+    const { participantType } = payload;
 
-        kickExternalTransferParticipant(payload);
-    
-      }
-    
+    if (
+      participantType !== "transfer" &&
+      participantType !== 'worker'
+    ) {
+
+      abortFunction();
+
+      kickExternalTransferParticipant(payload);
+
+    }
+
   })
 
+  Actions.addListener('beforeHangupCall', async (payload, abortFunction) => {
+    const { conference, sid, taskSid } = payload.task;
+    const participantsOnHold = (participant) => {
+      return participant.onHold == true
+    };
+    const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+    const getLatestConference = taskSid => {
+      const updatedTask = StateHelper.getTaskByTaskrouterTaskSid(taskSid)
+      return updatedTask.conference
+    }
+
+    // check if worker hanging up is last worker on the call
+    if (conference.liveWorkerCount === 1) {
+      //if so, ensure no other participants are on hold as 
+      //no external parties will be able to remove them from being on hold.
+      conference.participants.forEach(async (participant) => {
+        if (participant.onHold) {
+          await Actions.invokeAction("UnholdParticipant", {
+            sid,
+            participant
+          });
+        }
+      });
+
+      // make sure this operation blocks hanging up the call until 
+      // all participants are unhold
+      while (getLatestConference(taskSid).participants.some(participantsOnHold)) {
+        await snooze(200);
+      }
+    }
+  })
 }
