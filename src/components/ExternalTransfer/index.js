be5bf849d24727cee88554462dac5df1677f64a5
Sam Johnson <sam@scj.me>
2022-08-26 20:30:10 +0000

    Remove temp workaround for now

--- src/components/ExternalTransfer/index.js
+++ src/components/ExternalTransfer/index.js
@@ -33,8 +33,8 @@ export const loadExternalTransferInterface = (flex, manager) => {
 	flex.CallCanvas.Content.add(<ConferenceMonitor
 		key="conference-monitor"
 	/>, { sortOrder: 999 });
-// TODO TEMP FIX below
-	const isUnknownParticipant = props => (!props.participant.participantType || props.participant.participantType === 'unknown');
+
+	const isUnknownParticipant = props => props.participant.participantType === 'unknown';
 	const isNotTransferParticipant = props => props.participant.participantType !== 'transfer';
 
 	// This section is for the full width ParticipantCanvas
